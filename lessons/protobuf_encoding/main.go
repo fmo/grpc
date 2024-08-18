b20ee1ae450b8834bad0328a88ccf93f960e68ae
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-08-18 15:00:02 +0300

    encoding

--- lessons/protobuf_encoding/main.go
+++ lessons/protobuf_encoding/main.go
@@ -21,4 +21,12 @@ func main() {
 
 	// Print the serialized output as hexadecimal
 	fmt.Printf("Serialized message: %X\n", serializedMsg)
+
+	// Hex literal
+	hexLiteral := []byte{0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x0a}
+	fmt.Printf("Hex literal: %x\n", hexLiteral)
+
+	// UTF-8 string
+	utf8String := "Hello, Protobuf!"
+	fmt.Printf("UTF-8 string: %x\n", utf8String)
 }
